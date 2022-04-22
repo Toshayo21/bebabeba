3d496e72fe393cd6004849fb4cbd0f839c94b323
BuganSuyemin <55956947+BuganSuyemin@users.noreply.github.com>
2022-04-22 16:02:10 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+enum Tile
+{
+	UNKNOWN,
+	FILLED,
+	EMPTY
+};
+
+int main()
+{
+	int width, height;
+	vector<vector<Tile>> r;
+	vector<vector<int>> hintsH;
+	vector<vector<int>> hintsV;
+
+	return 0;
+}
