bb06eeafd7c47eeb907428fa92e16b14f16d2fd0
BuganSuyemin <55956947+BuganSuyemin@users.noreply.github.com>
2022-04-30 11:09:51 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
-#define SIZE 10
+#include <fstream>
+#include <sstream>
 using namespace std;
 
 enum Tile
@@ -16,9 +17,7 @@ bool isRowComplete(vector<vector<Tile>> r, vector<int> hints, int row, int width
 	int currentLen = 0;
 	int t = -1;
 	for (int i = 0; i < width; i++)
-	{
 		if (r[row][i] == FILLED)
-		{
 			if (empty)
 			{
 				empty = 0;
@@ -26,32 +25,16 @@ bool isRowComplete(vector<vector<Tile>> r, vector<int> hints, int row, int width
 				currentLen = 1;
 			}
 			else
-			{
 				currentLen++;
-			}
-		}
-		else
+		else if (!empty)
 		{
-			if (!empty)
-			{
-				//cout << currentLen << endl;
-				empty = 1;
-				if (!(hints[t] == currentLen))
-				{
-					return false;
-				}
-			}
+			empty = 1;
+			if (!(hints[t] == currentLen))
+				return false;
 		}
-	}
 
-	if (!empty)
-	{
-		//cout << currentLen << endl;;
-		if (!(hints[t] == currentLen))
-		{
-			return false;
-		}
-	}
+	if (!empty && !(hints[t] == currentLen))
+		return false;
 	return true;
 }
 
@@ -61,9 +44,7 @@ bool isColumnComplete(vector<vector<Tile>> r, vector<int> hints, int column, int
 	int currentLen = 0;
 	int t = -1;
 	for (int i = 0; i < height; i++)
-	{
 		if (r[i][column] == FILLED)
-		{
 			if (empty)
 			{
 				empty = 0;
@@ -71,32 +52,16 @@ bool isColumnComplete(vector<vector<Tile>> r, vector<int> hints, int column, int
 				currentLen = 1;
 			}
 			else
-			{
 				currentLen++;
-			}
-		}
-		else
+		else if (!empty)
 		{
-			if (!empty)
-			{
-				//cout << currentLen << endl;
-				empty = 1;
-				if (!(hints[t] == currentLen))
-				{
-					return false;
-				}
-			}
+			empty = 1;
+			if (!(hints[t] == currentLen))
+				return false;
 		}
-	}
 
-	if (!empty)
-	{
-		//cout << currentLen << endl;;
-		if (!(hints[t] == currentLen))
-		{
-			return false;
-		}
-	}
+	if (!empty && !(hints[t] == currentLen))
+		return false;
 	return true;
 }
 
@@ -110,73 +75,74 @@ bool isComplete(vector<vector<Tile>> r, vector<vector<int>> hintsH, vector<vecto
 		if (!isColumnComplete(r, hintsV[i], i, height))
 			return false;
 
-
 	return true;
+}
+
+void readFile(int& height, int& width, vector<vector<int>> &hintsH, vector<vector<int>> &hintsV)
+{
+	string str;
+	ifstream file("filenme.txt");
+	if (!file)
+	{
+		cout << "Error reading file";
+		exit(-1);
+	}
+	file >> height >> width;
+
+	vector<vector<int>> H(width, vector<int>());
+	vector<vector<int>> V(height, vector<int>());
+
+	getline(file, str);
+	for (int i = 0; i < height; i++)
+	{
+		getline(file, str);
+		istringstream ss(str);
+		int num;
+		while (ss >> num)
+			H[i].push_back(num);
+	}
+	for (int i = 0; i < width; i++)
+	{
+		getline(file, str);
+		istringstream ss(str);
+		int num;
+		while (ss >> num)
+			V[i].push_back(num);
+
+	}
+
+	hintsH = H;
+	hintsV = V;
+	
+	file.close();
 }
 
 int main()
 {
 	int width, height;
-	vector<vector<Tile>> r(5, vector<Tile>(5));
-	vector<vector<int>> hintsH(5, vector<int>(SIZE));
-	vector<vector<int>> hintsV(5, vector<int>(SIZE));
-
-	width = 5, height = 5;
-	for (int i = 0; i < width; i++)
-	{
-		for (int j = 0; j < height; j++)
-		{
-			r[i][j] = UNKNOWN;
-		}
-	}
-
-	//sample array
-	//----------------------
+	vector<vector<int>> hintsV;
+	vector<vector<int>> hintsH;
+	readFile(width, height, hintsH, hintsV);
+	vector<vector<Tile>> r(width, vector<Tile>(height));
+	
 	r[0][2] = FILLED;
-	r[0][3] = FILLED;
 	r[0][4] = FILLED;
 
-	r[1][0] = FILLED;
-	r[1][2] = FILLED;
+	r[1][3] = FILLED;
 	r[1][4] = FILLED;
 
-	r[2][0] = FILLED;
-	r[2][1] = FILLED;
+	r[2][3] = FILLED;
 	r[2][4] = FILLED;
 
 	r[3][0] = FILLED;
 	r[3][1] = FILLED;
+	r[3][3] = FILLED;
+	r[3][4] = FILLED;
 
 	r[4][0] = FILLED;
-	r[4][4] = FILLED;
+	r[4][1] = FILLED;
+	r[4][2] = FILLED;
 
-
-	hintsH[0][0] = 3;
-
-	hintsH[1][0] = 1;
-	hintsH[1][1] = 1;
-	hintsH[1][2] = 1;
-
-	hintsH[2][0] = 2;
-	hintsH[2][1] = 1;
-
-	hintsH[3][0] = 2;
-
-	hintsH[4][0] = 1;
-	hintsH[4][1] = 1;
-
-
-	hintsV[0][0] = 4;
-
-	hintsV[1][0] = 2;
-
-	hintsV[2][0] = 2;
-
-	hintsV[3][0] = 1;
-
-	hintsV[4][0] = 3;
-	hintsV[4][1] = 1;
-	//----------------------
 
 	cout << isComplete(r, hintsH, hintsV, height, width);
 
